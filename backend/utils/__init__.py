956d68689ecc4819cbde9b1b3fc755c4d427e8af
Rafsan1711 <samiulhaquerafsan@gmail.com>
2025-11-29 19:58:36 +0600

    Update __init__.py

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -5,5 +5,6 @@ Utility functions and helpers
 
 from .data_loader import DataLoader
 from .logger import setup_logger
+from services.firebase_service import FirebaseService
 
-__all__ = ['DataLoader', 'setup_logger']
+__all__ = ['DataLoader', 'setup_logger', 'FirebaseService']
